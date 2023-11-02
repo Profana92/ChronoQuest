2b52d8b32c1a3306b2da94fba68423229b3d1616
Profana92 <stevevtek@gmail.com>
2023-11-02 23:44:16 +0100

    Middleware added

--- client/middleware.tsx
+++ client/middleware.tsx
@@ -1,2 +1,23 @@
-export { default } from "next-auth/middleware";
+// export { default } from "next-auth/middleware";
+import { withAuth } from "next-auth/middleware";
+import { NextResponse } from "next/server";
+export default withAuth(
+  // `withAuth` augments your `Request` with the user's token.
+  function middleware(req) {
+    const { pathname, origin } = req.nextUrl;
+    const { token } = req.nextauth;
+    if (pathname.startsWith("/dashboard") && token?.user?.role !== "admin") {
+      //   return NextResponse.redirect(origin);
+      return new NextResponse("You are not authorized to view this content.");
+    }
+  },
+  {
+    callbacks: {
+      authorized: ({ token }) => {
+        return !!token;
+      },
+    },
+  }
+);
+
 export const config = { matcher: ["/profile/:path*", "/protected/:path*", "/dashboard/:path*"] };
